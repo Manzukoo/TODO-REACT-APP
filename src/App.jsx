50640543194233f194c9b7a7c1ae634fb1235d88
Manzukoo <bautistarucci2015@gmail.com>
2022-06-02 23:21:09 -0300

    Creando la estructura de la página

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import Boton from './components/Boton';
 function App() {
   return (
     <div className="App">
-      <div className="buttons">
+      <div className="todo-container">
+        <ul className="tasks">
+          
+        </ul>
+        <div className="buttons">
         <Boton
         btnName="Agregar" />
         <Boton
         btnName="Reiniciar" />
+      </div>
       </div>
     </div>
   );
