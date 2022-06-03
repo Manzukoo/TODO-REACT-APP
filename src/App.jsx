96fbadfec019efa5fe88586dd367009276c86717
Manzukoo <bautistarucci2015@gmail.com>
2022-06-02 23:23:27 -0300

    Creando el componenete para la tarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Boton from './components/Boton';
+import Task from './components/Task';
 
 function App() {
   return (
     <div className="App">
       <div className="todo-container">
         <ul className="tasks">
-          
+          <Task>Prueba1</Task>
+          <Task>Prueba2</Task>
+          <Task>Prueba3</Task>
         </ul>
         <div className="buttons">
         <Boton
---
+++ src/components/Task.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Task.css'
+
+const Task = ({ children }) => (
+	<li>
+		{children}
+  </li>
+)
+
+export default Task
