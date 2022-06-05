04d81031306febbedd078930970aedefa75d7794
Manzukoo <bautistarucci2015@gmail.com>
2022-06-05 17:43:54 -0300

    Añadiendo mas props a la tarea

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Task.css'
 import { AiOutlineCloseSquare } from 'react-icons/ai';
 
-const Task = ({ completed, children }) => (
+const Task = ({ id, completeTask, deleteTask, completed, children }) => (
 	<li className={`task ${completed ? 'completed' : ''}`.trimEnd()}>
 		<p className="text-of-task">
 			{children}
