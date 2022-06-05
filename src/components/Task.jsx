deb57e3b8fad71c9430ffe7b0d76036f3cc61636
Manzukoo <bautistarucci2015@gmail.com>
2022-06-05 18:51:23 -0300

    Añadiendo una caja para recibir el click para completar la tarea, porque o sino no deja darle click al botón de eliminar la tarea

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,14 +3,15 @@ import './Task.css'
 import { AiOutlineCloseSquare } from 'react-icons/ai';
 
 const Task = ({ id, completeTask, deleteTask, completed, children }) => (
-	<li onClick={() => completeTask(id)} className={`task ${completed ? 'completed' : ''}`.trimEnd()}>
+	<li className={`task ${completed ? 'completed' : ''}`.trimEnd()}>
 		<p className="text-of-task">
 			{children}
 		</p>
 		<div onClick={() => deleteTask(id)} className="delete-task">
 			<AiOutlineCloseSquare className="delete-task" />
 		</div>
-  </li>
+		<div onClick={() => completeTask(id)} className="completeTask"></div>
+	</li>
 )
 
 export default Task
