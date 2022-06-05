1db28397ec7c4fda2642aef5ccb4ec67d7abc618
Manzukoo <bautistarucci2015@gmail.com>
2022-06-05 17:43:06 -0300

    Creando el estado de las tareas y la renderización de tal

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import './Tasks.css'
 import Task from './Task';
 import InputTask from './InputTask';
 
-const Tasks = () => (
-  <>
-    <InputTask />
-    <div className="todo-container">
-      <ul className="tasks">
-        <Task
-          completed={true}>Prueba1</Task>
-        <Task
-          completed={false}>Prueba2</Task>
-        <Task
-          completed={false}>Prueba3</Task>
-      </ul>
-    </div>
-  </>
-)
+const Tasks = () => {
+  
+  const [tasks, setTasks] = useState([])
+
+  return (
+    <>
+      <InputTask />
+      <div className="todo-container">
+        <ul className="tasks">
+          {
+            tasks.map((task) =>
+              <Task completed={task.completed}> {task.text} </Task>
+
+            )
+          }
+        </ul>
+      </div>
+    </>
+  )
+}
 
 export default Tasks
